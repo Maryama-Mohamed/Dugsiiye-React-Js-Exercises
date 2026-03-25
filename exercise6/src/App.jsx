0f34b62fcfb883c476103fcee183d169b233f099
Maryama Mohamed <queenikhlaas50@gmail.com>
2026-03-25 15:57:26 -0300

    added exercise 6

---
+++ exercise6/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  
+const GreetingTitle = () => {
+  const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('Hello');
+
+  useEffect(() => {
+    if (!name) {
+      document.title = 'Welcome!';
+    } else {
+      document.title = `${greeting}, ${name}`;
+    }
+  }, [name, greeting]);
+}
+  return (
+    <>
+      <div>
+      <h2>Enter Your Name:</h2>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <h2>Choose a Greeting:</h2>
+      <input
+        type="text"
+        value={greeting}
+        onChange={(e) => setGreeting(e.target.value)}
+      />
+    </div>
+
+    </>
+  )
+}
+
+export default App
